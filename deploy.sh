bc873cbf96103b9d54563576fdcaca544b4a9328
Harsh Gupta <gupta.harsh96@gmail.com>
2016-04-10 16:45:20 +0530

    Deploy by new repo
    I found the methods using deployment by subtree pretty confusing and they were
    not working for me. Now what I have done is I have initiated another git repo
    in the output directory. The new script changes to that, add everythign and
    commits.
    
    Signed-off-by: Harsh Gupta <gupta.harsh96@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -4,13 +4,14 @@
 echo "Deploying to GitHub pages..."
 
 # Create the subtree
-git subtree split --prefix output -b gh-pages
+cd output
+# git subtree split --prefix output -b gh-pages
 # Push to origin
+git add *
+git commit -m "Build"
 git push -f origin gh-pages
 # Remove so that later the process can be repeated
-git branch -D gh-pages
 # Push master
-git push origin master
 
 # nikola github_deploy
 # git checkout master
