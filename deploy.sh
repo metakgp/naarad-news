fcfd4ba83f82ef128a5a9838da84060de7778197
Vivek Rai <vivekrai.iitkgp@gmail.com>
2016-08-18 21:53:36 +0530

    Fix deploy script
    Signed-off-by: Vivek Rai <vivekrai.iitkgp@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -6,15 +6,5 @@ python frontend.py
 # Ensure that you have rebuilt the site and commited your changes
 echo "Deploying to GitHub pages..."
 
-# Create the subtree
-cd output
-# git subtree split --prefix output -b gh-pages
-# Push to origin
-git add *
-git commit -m "Build"
-git push -f origin gh-pages
-# Remove so that later the process can be repeated
-# Push master
-
-# nikola github_deploy
-# git checkout master
+# Create the subtree, push changes to gh-pages
+git subtree push --prefix output origin gh-pages
