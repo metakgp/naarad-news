a179da234363e0754d5863f11162473a6c1bfc97
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2016-08-20 03:43:45 +0530

    Updated prettify_date function

--- fbscraper.py
+++ fbscraper.py
@@ -164,9 +164,9 @@ def utc_to_time(naive, timezone="Asia/Colombo"):
 
 def prettify_date(data):
   for i in range(0,len(data)):
-    date = data[i]['created_time'] 
-    p1 = string.split(date,"T")[0]
-    p2 = string.split(string.split(date,"T")[1],"+")[0]
+    date = data[i]['created_time']
+    p1 = date.split("T")[0]
+    p2 = date.split("T")[1].split("+")[0]
     date = parse(p1 + " " + p2)
     date = utc_to_time(date,"Asia/Colombo")
     time = datetime.strptime(date.strftime("%H%M"), '%H%M').strftime('%I:%M%p').upper()
@@ -193,7 +193,7 @@ def get_aggregated_feed(pages):
 
 if __name__ == "__main__":
     # Great thanks to https://gist.github.com/abelsonlive/4212647
-     news_pages = [('The Scholar\'s Avenue', 'scholarsavenue'),
+    news_pages = [('The Scholar\'s Avenue', 'scholarsavenue'),
                    ('Awaaz IIT Kharagpur', 'awaaziitkgp'),
                    ('Technology Students Gymkhana', 'TSG.IITKharagpur'),
                    ('Technology IIT KGP', 'iitkgp.tech')  ]
@@ -201,9 +201,7 @@ if __name__ == "__main__":
 
     data = get_aggregated_feed(news_pages)
     data = remove_duplicates(data)
-    try :
-        data = prettify_date(data)
-    except AttributeError :
-        pass 
+    data = prettify_date(data)
+
     json.dump(data, open('output/feed.json', 'w'))
     write_html(data, 'output/index.html')
