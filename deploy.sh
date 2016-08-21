b8d91a858e7adefe2ff734efe2fedd351ebacf8d
Vivek Rai <vivekrai.iitkgp@gmail.com>
2016-08-22 01:48:46 +0530

    Update deploy.sh to honor docs/ build
    Signed-off-by: Vivek Rai <vivekrai.iitkgp@gmail.com>

--- deploy.sh
+++ deploy.sh
@@ -5,14 +5,15 @@ python frontend.py
 
 # Commit changes
 echo "commiting.."
-git add output/
+git add -A
 git commit -m 'Update and Build'
 
 # Create the subtree, push changes to gh-pages
-git subtree split --prefix output -b gh-pages
+# git subtree split --prefix output -b gh-pages
 
 echo "deploying.."
-git push -f origin gh-pages:gh-pages
+git push origin master
+# git push -f origin gh-pages:gh-pages
 
-echo "cleaning..."
-git branch -D gh-pages
+# echo "cleaning..."
+# git branch -D gh-pages
