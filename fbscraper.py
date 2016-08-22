28814520fcae2454d98037e7b3c022b2e25dae8b
Ayush Goyal <ayushgoyal.iitkgp@gmail.com>
2016-08-22 22:06:38 +0530

    Fixes #30

--- fbscraper.py
+++ fbscraper.py
@@ -62,16 +62,20 @@ def get_link(post_id):
     return link
 
 
-def get_event(post_id) :
-    event_id = post_id.split('_')[1]  #getting only the event id from the post id
-    base_query = event_id + '/'
-    event_dict = graph.get(base_query)
-    DateTime = prettify_date([{'created_time': event_dict['start_time']}])
-    if 'description' in event_dict.keys() :  #checking if the event have description 
-    	message = "%s \nDate : %s\nTime : %s\nVenue : %s "  %  (event_dict['description'] ,DateTime[0]['real_time']  , DateTime[0]['real_date']  , event_dict['place']['name'])
-    else :
-    	message = "%s \nDate : %s\nTime : %s\nVenue : %s "  %  (event_dict['name'] ,DateTime[0]['real_date']  , DateTime[0]['real_time']  , event_dict['place']['name']) 
-    return message
+def get_event(post_id , page_id) :
+    base_query = page_id + '/events'
+    all_events = graph.get(base_query)
+    #print (all_events)
+    for event in all_events['data'] :
+        print (event)
+        if event['id'] in post_id :                
+            DateTime = prettify_date([{'created_time': event['start_time']}])
+            if 'description' in event.keys() :  #checking if the event have description 
+            	message = "%s \nDate : %s\nTime : %s\nVenue : %s "  %  (event['description'] ,DateTime[0]['real_time']  , DateTime[0]['real_date']  , event['place']['name'])
+            else :
+            	message = "%s \nDate : %s\nTime : %s\nVenue : %s "  %  (event['name'] ,DateTime[0]['real_date']  , DateTime[0]['real_time']  , event['place']['name']) 
+            return message
+
 
 
 def get_shared_post(post_id) :
@@ -85,7 +89,7 @@ def shortify_links(message) :
     
     max_string_length = 20
     show_string_length = 15
-    message = string.split(message," ")
+    message = message.split(" ")
     new_message = ""
     
     for mess in message :
@@ -155,7 +159,7 @@ def get_feed(page_id, pages=10):
         post_dict['link'] = get_link(post_dict['id'])
         try :  #Events and shared post have story key
             if "event" in post_dict['story'] :
-            	post_dict['message'] = get_event(post_dict['id'])
+            	post_dict['message'] = get_event(post_dict['id'] , page_id)
             elif "shared" in post_dict['story'] :
                 post_dict['message'] = ' <b> ' + post_dict['story'] + ' </b> ' + ' \n\n ' + get_shared_post(post_dict['id']) 
             post_dict['message'] = shortify_links(post_dict['message'])
